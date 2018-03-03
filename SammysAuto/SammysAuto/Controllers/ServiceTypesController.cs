b9882c0cb8e0f04caeb2e5a42e60bc15f5122882
Elizan <EJE0101@daugherty.com>
2018-03-03 18:25:22 -0500

    added files

---
+++ SammysAuto/SammysAuto/Controllers/ServiceTypesController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SammysAuto.Data;
+
+namespace SammysAuto.Controllers
+{
+    public class ServiceTypesController : Controller
+    {
+        // 1. db object 
+        private readonly ApplicationDbContext _db;
+
+        // 2. constructor 
+        public ServiceTypesController(ApplicationDbContext db)
+        {
+            this._db = db; 
+        }
+
+        public IActionResult Index()
+        {
+            // show the list of all the service types in db
+            return View(_db.ServiceTypes.ToList());
+        }
+
+        // 3. dispose _db 
+        protected override void Dispose(bool disposing)
+        {
+            if(disposing)
+            {
+                _db.Dispose(); 
+            }
+        }
+    }
+}
